6c211096bef33d9728da6575c2fd36cc98750fc4
Lowouis <83007122+Lowouis@users.noreply.github.com>
2022-11-02 09:06:45 +0100

    new pages added

--- folio/experience.php
+++ folio/experience.php
@@ -1,4 +1,82 @@
 <section id="experience" class="article-menu cat">
     <h1><a class="c-number">03.</a>Experience</h1>
+    <div class="card-glass">
+        <div class="inline">
+            <h2 class="mg-45">SAP LABS</h2>
+            <div class="col color-g mg-45">
+                <span>2022</span>
+                <span>2024</span>
+            </div>
+
+            <div class="inline">
+                <a>🧍</a>
+                <h3 class="mg-45">Support IT & Sofware Developer</h3>
+            </div>
+            <div class="inline">
+                <a>📍</a>
+                <h3 class="mg-45">Caen</h3>
+            </div>
+            <h4 class="c-light">Tâches réalisées</h4>
+
+        </div>
+        <hr class="line">
+        <div class="inline">
+            <h2 class="mg-45">SAP LABS</h2>
+            <div class="col color-g mg-45">
+                <span>2022</span>
+                <span>2024</span>
+            </div>
+
+            <div class="inline">
+                <a>🧍</a>
+                <h3 class="mg-45">Support IT & Sofware Developer</h3>
+            </div>
+            <div class="inline">
+                <a>📍</a>
+                <h3 class="mg-45">Caen</h3>
+            </div>
+            <h4 class="c-light">Tâches réalisées</h4>
+
+        </div>
+        <hr class="line">
+        <div class="inline">
+            <h2 class="mg-45">SAP LABS</h2>
+            <div class="col color-g mg-45">
+                <span>2022</span>
+                <span>2024</span>
+            </div>
+
+            <div class="inline">
+                <a>🧍</a>
+                <h3 class="mg-45">Support IT & Sofware Developer</h3>
+            </div>
+            <div class="inline">
+                <a>📍</a>
+                <h3 class="mg-45">Caen</h3>
+            </div>
+            <h4 class="c-light">Tâches réalisées</h4>
+
+        </div>
+        <hr class="line">
+        <div class="inline">
+            <h2 class="mg-45">SAP LABS</h2>
+            <div class="col color-g mg-45">
+                <span>2022</span>
+                <span>2024</span>
+            </div>
+
+            <div class="inline">
+                <a>🧍</a>
+                <h3 class="mg-45">Support IT & Sofware Developer</h3>
+            </div>
+            <div class="inline">
+                <a>📍</a>
+                <h3 class="mg-45">Caen</h3>
+            </div>
+            <h4 class="c-light">Tâches réalisées</h4>
+        </div>
+        <a target=”_blank” class="btn-nf ">Pas d'archive</a>
+    </div>
+
 
 </section>
