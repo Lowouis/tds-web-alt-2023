56172db275654173ed5c36bc2722e127f6539443
Lowouis <louisguritapro@gmail.com>
2022-11-23 01:20:50 +0100

    addons on new portfolio

---
+++ menu/vues/propos.php
@@ -0,0 +1,33 @@
+<div id="contact" class="full-hidden">
+    <div id="title_home_container">
+        <h1>CONTACT</h1>
+        <div class="contact__container">
+            <form action="?" method="POST">
+                <div class="f-col">
+                    <label class="label_contact">Prénom</label>
+                    <input class="input_contact" type="text" name="contact" placeholder="Entrez votre prénom">
+                </div>
+
+                <div class="f-col">
+                    <label class="label_contact">Nom</label>
+                    <input class="input_contact" type="text" name="contact" placeholder="Entrez votre nom">
+                </div>
+
+                <div class="f-col">
+                    <label class="label_contact">Adresse mail</label>
+                    <input class="input_contact" type="text" name="contact" placeholder="Entrez votre adresse mail">
+                </div>
+
+                <div class="f-col">
+                    <label class="label_contact">Message</label>
+                    <textarea class="input_contact" type="text" name="contact" placeholder="Entrez votre message"></textarea>
+                </div>
+
+                <div class="f-row">
+                    <span class="material-symbols-outlined send_btn">send<button hidden name="contact" type="submit"></button></span>
+
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
